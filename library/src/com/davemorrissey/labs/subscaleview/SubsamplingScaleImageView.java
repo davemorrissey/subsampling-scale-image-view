eb81d86cf94da4c6ddec6582c148bf211586beab
David Morrissey <davemorrissey@gmail.com>
2014-05-21 08:31:03 +0100

    Copyright declaration added

--- library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -1,4 +1,6 @@
 /*
+Copyright 2014 David Morrissey
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -11,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package com.davemorrissey.labs.subscaleview;
 
 import android.content.Context;
