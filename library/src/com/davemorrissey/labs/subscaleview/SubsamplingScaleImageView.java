2424a7cf8b7eeb00dad7d411b8f02ecc93dbe636
David Morrissey <davemorrissey@gmail.com>
2015-09-03 21:09:51 +0100

    Tweaked log message

--- library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ library/src/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -1628,7 +1628,7 @@ public class SubsamplingScaleImageView extends View {
                     cursor.close();
                 }
             } catch (Exception e) {
-                Log.w(TAG, "Could not get EXIF orientation of image");
+                Log.w(TAG, "Could not get orientation of image from media store");
             }
         } else if (sourceUri.startsWith(ImageSource.FILE_SCHEME) && !sourceUri.startsWith(ImageSource.ASSET_SCHEME)) {
             try {
