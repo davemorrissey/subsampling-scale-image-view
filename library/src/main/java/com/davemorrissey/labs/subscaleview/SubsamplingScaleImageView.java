2e714ab6a029fd2c05d22247834b48387d038bf8
Laurence Dawson <contact@laurencedawson.com>
2018-08-13 14:28:23 +0100

    If a preview bitmap was passed to SubsamplingScaleImageView and this bitmap was subsequently recycled, SubsamplingScaleImageView will still attempt to use this bitmap.
    This change (in addition to the existing null check) checks if the bitmap is not recycled before attempting to draw it.

--- library/src/main/java/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
+++ library/src/main/java/com/davemorrissey/labs/subscaleview/SubsamplingScaleImageView.java
@@ -1080,7 +1080,7 @@ public class SubsamplingScaleImageView extends View {
                 }
             }
 
-        } else if (bitmap != null) {
+        } else if (bitmap != null && !bitmap.isRecycled()) {
 
             float xScale = scale, yScale = scale;
             if (bitmapIsPreview) {
