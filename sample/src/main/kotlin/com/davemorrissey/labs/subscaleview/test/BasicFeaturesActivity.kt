8228660c2be9100fcb8fb75f4e75356d41355112
tibbi <tibor@kaputa.sk>
2019-01-20 21:01:49 +0100

    simplifying the sample project + cleanup and converting to kotlin

---
+++ sample/src/main/kotlin/com/davemorrissey/labs/subscaleview/test/BasicFeaturesActivity.kt
@@ -0,0 +1,15 @@
+package com.davemorrissey.labs.subscaleview.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.davemorrissey.labs.subscaleview.ImageSource
+import kotlinx.android.synthetic.main.pages_activity.*
+
+class BasicFeaturesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pages_activity)
+        actionBar?.title = getString(R.string.basic_title)
+        imageView.setImage(ImageSource.asset("sanmartino.jpg"))
+    }
+}
